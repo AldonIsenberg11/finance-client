7b9ae0900235d1c349b68e0c13d58cca9b5b8de7
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2022-08-11 01:26:50 -0500

    setup initial layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,129 +1,61 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
-<template>
-  <!--
-    This example requires updating your template:
+<script lang="ts" async setup>
+import { ArrowSmDownIcon, ArrowSmUpIcon } from '@heroicons/vue/solid'
+import { inspect } from 'util';
 
-    ```
-    <html class="h-full bg-gray-100">
-    <body class="h-full">
-    ```
-  -->
-  <div class="min-h-full">
-    <div class="bg-gray-800 pb-32">
-      <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="border-b border-gray-700">
-            <div class="flex items-center justify-between h-16 px-4 sm:px-0">
-              <div class="flex items-center">
-                <div class="flex-shrink-0">
-                  <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
-                </div>
-                <div class="hidden md:block">
-                  <div class="ml-10 flex items-baseline space-x-4">
-                    <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
-                  </div>
-                </div>
-              </div>
-              <div class="hidden md:block">
-                <div class="ml-4 flex items-center md:ml-6">
-                  <button type="button" class="bg-gray-800 p-1 text-gray-400 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                    <span class="sr-only">View notifications</span>
-                    <BellIcon class="h-6 w-6" aria-hidden="true" />
-                  </button>
+const { data, pending, error, refresh } = await useFetch('http://localhost:6060/stockBreakdown')
 
-                  <!-- Profile dropdown -->
-                  <Menu as="div" class="ml-3 relative">
-                    <div>
-                      <MenuButton class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                        <span class="sr-only">Open user menu</span>
-                        <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
-                      </MenuButton>
-                    </div>
-                    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                      <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                          <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
-                        </MenuItem>
-                      </MenuItems>
-                    </transition>
-                  </Menu>
-                </div>
-              </div>
-              <div class="-mr-2 flex md:hidden">
-                <!-- Mobile menu button -->
-                <DisclosureButton class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                  <span class="sr-only">Open main menu</span>
-                  <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-                  <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
-                </DisclosureButton>
-              </div>
-            </div>
-          </div>
-        </div>
+let stock = data.value[0];
 
-        <DisclosurePanel class="border-b border-gray-700 md:hidden">
-          <div class="px-2 py-3 space-y-1 sm:px-3">
-            <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
-          </div>
-          <div class="pt-4 pb-3 border-t border-gray-700">
-            <div class="flex items-center px-5">
-              <div class="flex-shrink-0">
-                <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
-              </div>
-              <div class="ml-3">
-                <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
-                <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
-              </div>
-              <button type="button" class="ml-auto bg-gray-800 flex-shrink-0 p-1 text-gray-400 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                <span class="sr-only">View notifications</span>
-                <BellIcon class="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div class="mt-3 px-2 space-y-1">
-              <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">{{ item.name }}</DisclosureButton>
-            </div>
-          </div>
-        </DisclosurePanel>
-      </Disclosure>
-      <header class="py-10">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold text-white">Dashboard</h1>
-        </div>
-      </header>
-    </div>
+// try {
+//   stock = JSON.parse(stock)
+// } catch(e) {
+//   stock = `Error parsing data: ${inspect(e)}`
+// }
 
-    <main class="-mt-32">
-      <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
-        <div class="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
-          <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-        </div>
-        <!-- /End replace -->
-      </div>
-    </main>
-  </div>
-</template>
+// const data = await $fetch('http://localhost:6060/stockBreakdown')
+// const { data, pending, error, refresh } = await useFetch(
+//   'http://localhost:6060/stockBreakdown',
+//   {
+//     headers: {
+//       'Accept': 'application/json',
+//     }
+//   }
+// )
 
-<script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+const latestUpdate = new Date(stock.date).toDateString()
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
+const stats = [
+  // { name: 'Latest Update', stat: new Date(stock.date).toDateString(), previousStat: null, change: null, changeType: null },
+  { name: '5 year score', stat: '58.16%', previousStat: '56.14%', change: '2.02%', changeType: 'increase' },
+  { name: '3 year score', stat: '48.16%', previousStat: '36.14%', change: '3.02%', changeType: 'increase' },
+  { name: '1 year score', stat: '24.57%', previousStat: '28.62%', change: '4.05%', changeType: 'decrease' },
 ]
 </script>
+
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div>
+    <h3 class="text-lg leading-6 font-medium text-gray-900">Default View [{{stock.ticker}}]</h3>
+    <h3 class="text-lg leading-6 font-medium text-gray-900">Latest Update [{{latestUpdate}}]</h3>
+    <dl class="mt-5 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x">
+      <div v-for="item in stats" :key="item.name" class="px-4 py-5 sm:p-6">
+        <dt class="text-base font-normal text-gray-900">
+          {{ item.name }}
+        </dt>
+        <dd class="mt-1 flex justify-between items-baseline md:block lg:flex">
+          <div class="flex items-baseline text-2xl font-semibold text-indigo-600">
+            {{ item.stat }}
+            <span class="ml-2 text-sm font-medium text-gray-500"> from {{ item.previousStat }} </span>
+          </div>
+
+          <div :class="[item.changeType === 'increase' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800', 'inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0']">
+            <ArrowSmUpIcon v-if="item.changeType === 'increase'" class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500" aria-hidden="true" />
+            <ArrowSmDownIcon v-else class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-red-500" aria-hidden="true" />
+            <span class="sr-only"> {{ item.changeType === 'increase' ? 'Increased' : 'Decreased' }} by </span>
+            {{ item.change }}
+          </div>
+        </dd>
+      </div>
+    </dl>
+  </div>
+</template>
